9502d130302a7701aee0f3b4ae7d1457fbac92ab
sp574 <sp574@cornell.edu>
2015-01-11 14:25:05 -0500

    -- Front "Calculator" fragment is done -- Need to implement DB, Save-Float-Button, Percentage-Float-Fragment, History ListView

--- app/src/main/java/sleeping_vityaz/onerm/Calculations.java
+++ app/src/main/java/sleeping_vityaz/onerm/Calculations.java
@@ -1,10 +1,12 @@
 package sleeping_vityaz.onerm;
 
+import android.util.Log;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
- * Created by naja-ox on 1/10/15.
+ * Created by naja-ox
  */
 public class Calculations {
 
@@ -12,8 +14,27 @@ public class Calculations {
         return round((weight*reps/30+weight),1);
     }
 
-    public static double calculateConservativeRepMax(double weight, int reps){
+    /*public static double calculateConservativeRepMax(double weight, int reps){
         return round(((weight*36)/(37-reps)),1);
+    }*/
+
+    public static double calculateXRM(double weight, int reps){
+        switch (reps){
+            case 1: return weight;
+            case 2: return round(0.95*weight,1);
+            case 3: return round(0.90*weight,1);
+            case 4: return round(0.88*weight,1);
+            case 5: return round(0.86*weight,1);
+            case 6: return round(0.83*weight,1);
+            case 7: return round(0.80*weight,1);
+            case 8: return round(0.78*weight,1);
+            case 9: return round(0.76*weight,1);
+            case 10: return round(0.75*weight,1);
+            case 11: return round(0.72*weight,1);
+            case 12: return round(0.70*weight,1);
+        }
+        Log.e("CALCULATIONS", "ERROR: Calculations shouldn't get to this point");
+        return weight;
     }
 
     private static double round(double value, int places) {
--- app/src/main/java/sleeping_vityaz/onerm/tabsswipe/CalculatorFragment.java
+++ app/src/main/java/sleeping_vityaz/onerm/tabsswipe/CalculatorFragment.java
@@ -4,31 +4,39 @@ package sleeping_vityaz.onerm.tabsswipe;
 import sleeping_vityaz.onerm.Calculations;
 import sleeping_vityaz.onerm.R;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 
 
 /**
- * Created by naja-ox on 1/10/15.
+ * Created by naja-ox
  */
 public class CalculatorFragment extends Fragment {
 
-    private TextView tv_weight, tv_reps,
-            tv_your_onerm, tv_one_rm,
+    private TextView tv_reps,
+            tv_your_onerm,
             tv_onerm_big,
             tv_allrepmax,
             tv_your_xrm, tv_xrm,
             tv_xrm_big;
+
+    private FrameLayout touchInterceptor;
+
 
     private EditText et_weight;
     private DiscreteSeekBar discreteSeekBar_reps;
@@ -45,25 +53,53 @@ public class CalculatorFragment extends Fragment {
 
         findViewsById(rootView);
 
-        et_weight.requestFocus();
+        et_weight.addTextChangedListener(watch);
 
+        touchInterceptor.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (et_weight.isFocused()) {
+                        Rect outRect = new Rect();
+                        et_weight.getGlobalVisibleRect(outRect);
+                        if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                            et_weight.clearFocus();
+                            InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                        }
+                    }
+                }
+                return false;
+            }
+        });
+
+        tv_reps.setVisibility(TextView.INVISIBLE);
+        discreteSeekBar_reps.setVisibility(TextView.INVISIBLE);
+        tv_onerm_big.setVisibility(TextView.INVISIBLE);
+        tv_your_onerm.setVisibility(TextView.INVISIBLE);
+
+        tv_allrepmax.setVisibility(TextView.INVISIBLE);
+        discreteSeekBar_allrepmax.setVisibility(TextView.INVISIBLE);
+        tv_your_xrm.setVisibility(TextView.INVISIBLE);
+        tv_xrm.setVisibility(TextView.INVISIBLE);
+        tv_xrm_big.setVisibility(TextView.INVISIBLE);
 
         discreteSeekBar_reps.setNumericTransformer(new org.adw.library.widgets.discreteseekbar.DiscreteSeekBar.NumericTransformer() {
             @Override
             public int transform(int value) {
                 Log.d("SeekBar REPS", "" + weight + " Calculations 1RM = " + Calculations.calculateRepMax(weight, value));
 
-                if (value==1){
-                    tv_reps.setText("  1 Rep "+getResources().getString(R.string.tv_reps));
-                }else{
-                    tv_reps.setText(value+" Reps "+getResources().getString(R.string.tv_reps));
+                if (value == 1) {
+                    tv_reps.setText("1 Rep " + getResources().getString(R.string.tv_reps));
+                } else if (value == 0) {
+                    tv_reps.setText("Reps " + getResources().getString(R.string.tv_reps));
+                } else {
+                    tv_reps.setText(value + " Reps " + getResources().getString(R.string.tv_reps));
                 }
 
-                if (et_weight.getText().toString().equals("")) {
-                    tv_onerm_big.setText("");
-                } else {
-                    weight = Double.parseDouble(et_weight.getText().toString());
+                if (!et_weight.getText().toString().equals("")) {
                     tv_onerm_big.setText("" + Calculations.calculateRepMax(weight, value));
+                    tv_xrm_big.setText("" + Calculations.calculateXRM(Double.parseDouble(tv_onerm_big.getText().toString()), discreteSeekBar_allrepmax.getProgress()));
                 }
                 return value;
             }
@@ -73,15 +109,63 @@ public class CalculatorFragment extends Fragment {
             @Override
 
             public int transform(int value) {
-                return value * 1;
+                tv_allrepmax.setText(value+" "+getResources().getString(R.string.tv_repmax));
+                tv_xrm.setText(" "+value+" "+getResources().getString(R.string.tv_repmax));
+                if (!et_weight.getText().toString().equals("")) {
+                    tv_xrm_big.setText("" + Calculations.calculateXRM(Double.parseDouble(tv_onerm_big.getText().toString()), value));
+                    }
+                return value;
             }
         });
 
         return rootView;
     }
 
+    TextWatcher watch = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!et_weight.getText().toString().equals("")) {
+                weight = Double.parseDouble(et_weight.getText().toString());
+                tv_reps.setVisibility(TextView.VISIBLE);
+                discreteSeekBar_reps.setVisibility(TextView.VISIBLE);
+                tv_onerm_big.setVisibility(TextView.VISIBLE);
+                tv_your_onerm.setVisibility(TextView.VISIBLE);
+                tv_allrepmax.setVisibility(TextView.VISIBLE);
+                discreteSeekBar_allrepmax.setVisibility(TextView.VISIBLE);
+                tv_your_xrm.setVisibility(TextView.VISIBLE);
+                tv_xrm.setVisibility(TextView.VISIBLE);
+                tv_xrm_big.setVisibility(TextView.VISIBLE);
+                tv_onerm_big.setText("" + Calculations.calculateRepMax(weight, discreteSeekBar_reps.getProgress()));
+                tv_xrm_big.setText("" + Calculations.calculateXRM(Double.parseDouble(tv_onerm_big.getText().toString()), discreteSeekBar_allrepmax.getProgress()));
+
+            } else {
+                tv_reps.setVisibility(TextView.INVISIBLE);
+                discreteSeekBar_reps.setVisibility(TextView.INVISIBLE);
+                tv_onerm_big.setVisibility(TextView.INVISIBLE);
+                tv_your_onerm.setVisibility(TextView.INVISIBLE);
+                tv_allrepmax.setVisibility(TextView.INVISIBLE);
+                discreteSeekBar_allrepmax.setVisibility(TextView.INVISIBLE);
+                tv_your_xrm.setVisibility(TextView.INVISIBLE);
+                tv_xrm.setVisibility(TextView.INVISIBLE);
+                tv_xrm_big.setVisibility(TextView.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
     private void findViewsById(View view) {
-        tv_weight = (TextView) view.findViewById(R.id.tv_weight);
+
+        touchInterceptor = (FrameLayout) view.findViewById(R.id.touchInterceptor);
+
         et_weight = (EditText) view.findViewById(R.id.et_weight);
         tv_reps = (TextView) view.findViewById(R.id.tv_reps);
         tv_your_onerm = (TextView) view.findViewById(R.id.tv_your_onerm);
